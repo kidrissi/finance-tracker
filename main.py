07b9c8034c5b69eea89387a6f880899a9451066c
kidrissi <k.atiab.idrissi@gmail.com>
2024-07-12 16:10:24 +0100

    finish CSV

--- main.py
+++ main.py
@@ -14,16 +14,21 @@ class CSV:
         except FileNotFoundError:
             df = pd.DataFrame(columns=cls.COLOUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
-        
+
     @classmethod
     def add_entry(cls, date, amount, category, description):
+        cls.initialize_csv()
         new_entry = {
             "date": date,
             "amount": amount,
             "category": category,
             "description": description,
         }
-        
-        with open(cls.CSV_FILE, "a", newline=""):
-            writer = csv.
 
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLOUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully")
+
+
+CSV.add_entry("22-12-2023", 123, "depense", "buy food")
